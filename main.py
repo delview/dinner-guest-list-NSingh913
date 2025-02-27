715e4e232168a42e5a02472c89ca8379e2d8db6a
NSingh913 <nirbhais626@deltalearns.ca>
2025-02-27 09:05:27 -0800

    pseudocoded

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # This lists will invite 6 pepole to dinner and of them would not be able to make it so also need to send request to someone else
+# Create a function 
+# Greet the user
+# Ask the user how many people they want to invite
+# Ask for the names of the people 
+# Print a letter for each of them 
+# Ask the user if they want to add or remove anyone from the invitation
+# Print the new letter for the user
+# 
