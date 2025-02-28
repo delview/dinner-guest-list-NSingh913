39d0a84ebd751c4ef40f1a2a8f99d85f5cb1e429
NSingh913 <nirbhais626@deltalearns.ca>
2025-02-28 10:37:33 -0800

    created a function

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
 # This lists will invite 6 pepole to dinner and of them would not be able to make it so also need to send request to someone else
-# Create a function 
+
+
+# Create a function
+def send_invitations(guests_list):
+    print("Dinner Invitations: ")
+    for guest in guests_list:
+        print(f"Dear {guest}, you are invited to the dinner party!. ")
+
+  
+ 
 # Greet the user
+
+
+
+
 # Ask the user how many people they want to invite
+
+
+
+
 # Ask for the names of the people 
+
+
+
+
 # Print a letter for each of them 
+
+
+
+
 # Ask the user if they want to add or remove anyone from the invitation
+
+
+
+
 # Print the new letter for the user
+
+
+
+
 # 
