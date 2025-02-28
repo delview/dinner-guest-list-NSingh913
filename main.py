5123ed088aa43ca65e313646adc703dbcd9e4efd
NSingh913 <nirbhais626@deltalearns.ca>
2025-02-28 10:59:44 -0800

    asked for the number of people

--- main.py
+++ main.py
@@ -5,22 +5,28 @@
 def send_invitations(guests_list):
     print("Dinner Invitations: ")
     for guest in guests_list:
-        print(f"Dear {guest}, you are invited to the dinner party!. ")
+        print(f"Dear {guest}, you are invited to the dinner party! ")
 
   
  
 # Greet the user
-
+print("Welcome to the dinner inviting porgram! ")
 
 
 
 # Ask the user how many people they want to invite
+num_guests = int(input("How many people do you want to invite to the dinner party?: "))
+except error:
+print("Error input noe available only 6 guests per time allowed! ")
+num_guests = 6
 
+guests = 
 
 
 
 # Ask for the names of the people 
-
+for i in range(num_guests):
+    
 
 
 
