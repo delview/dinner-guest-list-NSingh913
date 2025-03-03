6bc00109644ef9b4ef40d4815f1540b555388e34
NSingh913 <nirbhais626@deltalearns.ca>
2025-03-02 19:48:03 -0800

    trying to loop

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 # This lists will invite 6 pepole to dinner and of them would not be able to make it so also need to send request to someone else
 
 
-# Create a function
+# Create a function to print invitations 
 def send_invitations(guests_list):
-    print("Dinner Invitations: ")
+
+    """Prints personalized invitation messages for each guests."""
+
+    print("\nDinner Invitations: ")
     for guest in guests_list:
         print(f"Dear {guest}, you are invited to the dinner party! ")
 
-  
- 
 # Greet the user
 print("Welcome to the dinner inviting program! ")
 
-
-
 # Ask the user how many people they want to invite
-num_guests = int(input("How many people do you want to invite to the dinner party?: "))
-except error:
-print("Error input noe available only 6 guests per time allowed! ")
-num_guests = 6
+while True:
+    try:
+        num_guests = int(input("How many people do you want to invite to the dinner party?: "))
+        except error:
+        print("Error input noe available only 6 guests per time allowed! ")
+        num_guests = 6
 
 guests = 
 
